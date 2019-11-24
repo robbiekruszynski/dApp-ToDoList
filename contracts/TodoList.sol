154b71101a41a4f17f6e29455b6db33b963b5b29
robbie <robbiekruszynski@gmail.com>
2019-11-23 22:22:12 -0800

    can render information, but having issues connecting to the right address after metamask

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,30 +1,22 @@
 pragma solidity ^0.5.0;
 
 contract TodoList {
-    //make a state variable
-    //state variables are written to the blockchain
-    uint public taskCount = 0;
+  uint public taskCount = 0;
 
-    struct Task {
-        uint id;
-        string content;
-        bool completed;
-    }
-//access the storage by making a new state variable 
-//mapping takes key value pair we declare the data type = unint and the task (Task)
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
 
-//making it public give it a reader function
-//mapping is a dynamic size you can't return the entire thing
-//that is why we use taskCount 
-    mapping(uint => Task) public tasks;
+  mapping(uint => Task) public tasks;
 
-    constructor()public{
-        createTask("Test task to check rendering");
-    }
-
-    function createTask(string memory _content) public {
-        taskCount ++;
-        tasks[taskCount] = Task(taskCount, _content, false);
-    }
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
+    constructor() public {
+        createTask("test task for rendering");
+      }
 
 }
